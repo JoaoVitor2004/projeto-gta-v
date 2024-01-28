f7513ced188e2f5c27d6d34c1481335b32651df0
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-01-27 23:39:14 -0300

    adicionei os arquivos do projeto

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+var plataformas = document.querySelector('ul.lista-de-plataformas1')
+
+        function selecionar() {
+            plataformas.style.display = 'block'
+        }
+
+        function sair() {
+            plataformas.style.display = 'none'
+        }
